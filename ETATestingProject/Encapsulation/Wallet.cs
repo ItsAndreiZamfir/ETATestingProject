ef37d6f2301620fcffdfb91ee48a8968df1c707d
Andrei Zamfir <Andrei.Zamfir@endava.com>
2025-05-14 13:51:38 +0300

    Adding the code for encapsulation

---
+++ ETATestingProject/Encapsulation/Wallet.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETATestingProject.Encapsulation
+{
+    public class Wallet
+    {
+        private decimal balance;
+
+        public Wallet(decimal initialBalance)
+        {
+            if (initialBalance < 0)
+            {
+                throw new ArgumentException("Initial balance cannot be negative.");
+            }
+            balance = initialBalance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Deposit amount must be positive.");
+            }
+            balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Withdrawal amount must be positive.");
+            }
+            if (amount > balance)
+            {
+                throw new InvalidOperationException("Insufficient funds.");
+            }
+            balance -= amount;
+        }
+
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+
+        public decimal Balance
+        {
+            get { return balance; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Balance cannot be negative.");
+                }
+                balance = value;
+            }
+        }
+    }
+}
---
+++ ETATestingProject/Encapsulation/WalletTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETATestingProject.Encapsulation
+{
+    public class WalletTest
+    {
+        Wallet wallet;
+
+        [SetUp]
+        public void Setup()
+        {
+            wallet = new Wallet(1000m);
+        }
+
+        [Test]
+        public void TestDeposit()
+        {
+            wallet.Deposit(500m);
+            Assert.That(wallet.GetBalance(), Is.EqualTo(1500m));
+            Assert.That(wallet.Balance, Is.EqualTo(1500m));
+        }
+
+        [Test]
+        public void TestWithdraw()
+        {
+            wallet.Withdraw(300m);
+            Assert.That(wallet.GetBalance(), Is.EqualTo(700m));
+            Assert.That(wallet.Balance, Is.EqualTo(700m));
+        }
+    }
+}
